ec2281dc26f25f4c573cf4a7c93c843476df2c39
dumpstertree <dumpstertree@gmail.com>
2016-10-15 14:19:55 -0400

    updated  start screen

---
+++ Cold Snap/Assets/TextAnimation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextAnimation : MonoBehaviour {
+
+	public float speed;
+	public Vector2 minScale;
+	public Vector2 maxScale;
+
+	public float maxRotation;
+	public float minRotation;
+
+
+	private void Update () {
+
+		float sinValue = Mathf.Abs( Mathf.Sin( Time.time * speed ) );
+		float newX = minScale.x + ((maxScale.x - minScale.x)*sinValue);
+		float newY = minScale.y + ((maxScale.y - minScale.y)*sinValue);
+
+		float newRotation = minRotation + ((maxRotation - minRotation)*sinValue);
+		gameObject.transform.localScale = new Vector3( newX, newY, 1);
+		gameObject.transform.localEulerAngles =  new Vector3(0,0,newRotation); 
+	}
+}
