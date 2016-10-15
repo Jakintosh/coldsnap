29f78f6d122c505daff1d24160bbdee4ff1eccff
dumpstertree <dumpstertree@gmail.com>
2016-10-15 08:01:46 -0400

    Updated art assets

--- Cold Snap/Assets/Scripts/Environment/LevelLoader.cs
+++ Cold Snap/Assets/Scripts/Environment/LevelLoader.cs
@@ -96,18 +96,110 @@ public class LevelLoader {
 	private enum TileType {
 		INVALID = -1,
 		DEFAULT,
+
+		ICEBLOCK,
+		STONEBLOCK_0,
+		FILLERBLOCK,
+
+		GROUNDDIRTSNOW_0,
+		GROUNDDIRTSNOW_1,
+
+		CEILING_0,
+		CEILING_1,
+		CEILING_2,
+
+		WALLLEFT_0,
+		WALLLEFT_1,
+		WALLLEFT_2,
+
+		WALLRIGHT_0,
+		WALLRIGHT_1,
+		WALLRIGHT_2,
+
+		CHAINTOP,
+		CHAINMID,
+		CHAINBOTTOM,
+
+		SNOWTOPLEFT,
+		SNOWTOPRIGHT,
+		TORCH,
+		SNOWBUMP,
+		SNOWDRIFTLEFT,
+		SNOWDRIFTRIGHT,
+
 		NUM_TILES
 	}
 
 	// map chars to tiles
 	private Dictionary<char,TileType> _tileSymbols = new Dictionary<char, TileType> () {
-		{ '0', TileType.DEFAULT }
+		{ '0', TileType.DEFAULT },
+
+		{ 'I', TileType.ICEBLOCK },
+		{ 'S', TileType.STONEBLOCK_0 },
+		{ 'F', TileType.FILLERBLOCK },
+
+		{ 'n', TileType.GROUNDDIRTSNOW_0 },
+		{ 'm', TileType.GROUNDDIRTSNOW_1 },
+
+		{ 't', TileType.CEILING_0 },
+		{ 'y', TileType.CEILING_1 },
+		{ 'u', TileType.CEILING_2 },
+
+		{ 'r', TileType.WALLRIGHT_0 },
+		{ 'f', TileType.WALLRIGHT_1 },
+		{ 'v', TileType.WALLRIGHT_2 },
+
+		{ 'e', TileType.WALLLEFT_0 },
+		{ 'd', TileType.WALLLEFT_1 },
+		{ 'c', TileType.WALLLEFT_2 },
+
+		{ 'T', TileType.CHAINTOP },
+		{ 'i', TileType.CHAINMID },
+		{ 'L', TileType.CHAINBOTTOM },
+
+		{ 'o', TileType.SNOWTOPLEFT },
+		{ 'p', TileType.SNOWTOPRIGHT },
+		{ 'l', TileType.TORCH },
+		{ '-', TileType.SNOWBUMP },
+		{ '<', TileType.SNOWDRIFTLEFT },
+		{ '>', TileType.SNOWDRIFTRIGHT }
+
 	};
 
 	// map tiles to prefabs
 	private Dictionary<TileType, GameObject> _tilePrefabs = new Dictionary<TileType, GameObject> () {
 		{ TileType.DEFAULT, Game.Resources.Environment.Tiles.Default },
-		{ TileType.INVALID, Game.Resources.Environment.Tiles.Invalid }
+		{ TileType.INVALID, Game.Resources.Environment.Tiles.Invalid },
+
+		{ TileType.STONEBLOCK_0, Game.Resources.Environment.Tiles.StoneBlock_0 },
+		{ TileType.ICEBLOCK, Game.Resources.Environment.Tiles.IceBlock },
+		{ TileType.FILLERBLOCK, Game.Resources.Environment.Tiles.fillerBlock },
+
+		{ TileType.GROUNDDIRTSNOW_0, Game.Resources.Environment.Tiles.GroundDirtSnow_0 },
+		{ TileType.GROUNDDIRTSNOW_1, Game.Resources.Environment.Tiles.GroundDirtSnow_1 },
+
+		{ TileType.CEILING_0, Game.Resources.Environment.Tiles.Ceiling_0 },
+		{ TileType.CEILING_1, Game.Resources.Environment.Tiles.Ceiling_1 },
+		{ TileType.CEILING_2, Game.Resources.Environment.Tiles.Ceiling_2 },
+
+		{ TileType.WALLRIGHT_0, Game.Resources.Environment.Tiles.WallRight_0 },
+		{ TileType.WALLRIGHT_1, Game.Resources.Environment.Tiles.WallRight_1 },
+		{ TileType.WALLRIGHT_2, Game.Resources.Environment.Tiles.WallRight_2 },
+
+		{ TileType.WALLLEFT_0, Game.Resources.Environment.Tiles.WallLeft_0 },
+		{ TileType.WALLLEFT_1, Game.Resources.Environment.Tiles.WallLeft_1 },
+		{ TileType.WALLLEFT_2, Game.Resources.Environment.Tiles.WallLeft_2 },
+
+		{ TileType.CHAINTOP, Game.Resources.Environment.Tiles.ChainTop },
+		{ TileType.CHAINMID, Game.Resources.Environment.Tiles.ChainMid },
+		{ TileType.CHAINBOTTOM, Game.Resources.Environment.Tiles.ChainBottom },
+
+		{ TileType.SNOWTOPLEFT, Game.Resources.Environment.Tiles.SnowTopLeft },
+		{ TileType.SNOWTOPRIGHT, Game.Resources.Environment.Tiles.SnowTopRight },
+		{ TileType.TORCH, Game.Resources.Environment.Tiles.Torch },
+		{ TileType.SNOWBUMP, Game.Resources.Environment.Tiles.SnowBump },
+		{ TileType.SNOWDRIFTLEFT, Game.Resources.Environment.Tiles.SnowDriftLeft },
+		{ TileType.SNOWDRIFTRIGHT, Game.Resources.Environment.Tiles.SnowDriftRight }
 	};
 
 	private void SpawnTile ( TileType type, Vector2 position ) {
@@ -124,7 +216,7 @@ public class LevelLoader {
 		// get position
 		var x = position.x + 0.5f;
 		var y = position.y + 0.5f;
-		var pos = new Vector3( x, y );
+		var pos = new Vector3( x, y, prefab.transform.position.z );
 
 		var tileGO = Object.Instantiate( prefab, pos, Quaternion.identity ) as GameObject;
 		tileGO.transform.SetParent( Game.View.Environment );
--- Cold Snap/Assets/Scripts/Game/Resources.cs
+++ Cold Snap/Assets/Scripts/Game/Resources.cs
@@ -5,8 +5,47 @@ public class Resources : MonoBehaviour {
 	[System.Serializable] public struct Environment_Resources {
 
 		[System.Serializable] public struct Tile_Resources {
+
 			[SerializeField] public GameObject Default;
-			[SerializeField] public GameObject Invalid;			
+			[SerializeField] public GameObject Invalid;
+
+			[Space]
+			[SerializeField] public GameObject IceBlock;	
+			[SerializeField] public GameObject StoneBlock_0;	
+			[SerializeField] public GameObject fillerBlock;
+
+			[Space]
+			[SerializeField] public GameObject GroundDirtSnow_0;
+			[SerializeField] public GameObject GroundDirtSnow_1;	
+
+			[Space]
+			[SerializeField] public GameObject WallRight_0;
+			[SerializeField] public GameObject WallRight_1;
+			[SerializeField] public GameObject WallRight_2;
+
+			[Space]
+			[SerializeField] public GameObject WallLeft_0;
+			[SerializeField] public GameObject WallLeft_1;
+			[SerializeField] public GameObject WallLeft_2;
+
+			[Space]
+			[SerializeField] public GameObject Ceiling_0;
+			[SerializeField] public GameObject Ceiling_1;
+			[SerializeField] public GameObject Ceiling_2;
+
+			[Space]
+			[SerializeField] public GameObject ChainTop;
+			[SerializeField] public GameObject ChainMid;
+			[SerializeField] public GameObject ChainBottom;
+
+			[Space]
+			[SerializeField] public GameObject SnowTopLeft;
+			[SerializeField] public GameObject SnowTopRight;
+			[SerializeField] public GameObject Torch;
+			[SerializeField] public GameObject SnowBump;
+			[SerializeField] public GameObject SnowDriftLeft;
+			[SerializeField] public GameObject SnowDriftRight;
+
 		}
 
 		[SerializeField] public Tile_Resources Tiles;
